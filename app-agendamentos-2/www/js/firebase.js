22052fc90f12acc400bb32691fbcca83a1b53572
Luisa Luz <83250828+LouWilly@users.noreply.github.com>
2021-11-08 17:27:40 -0300

    Add files via upload

---
+++ app-agendamentos-2/www/js/firebase.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyCE5T1eeouPM44OrCFuKXabspAL7dQuofc",
+    authDomain: "app-agendamentos-pam.firebaseapp.com",
+    projectId: "app-agendamentos-pam",
+    storageBucket: "app-agendamentos-pam.appspot.com",
+    messagingSenderId: "920484435312",
+    appId: "1:920484435312:web:58b8925bf1fb771c1cebb5",
+    measurementId: "G-08LS95WSF0"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
